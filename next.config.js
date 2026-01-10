6dbe1d245c9118b155b7b67f3f73c4f8fd9598d3
zebvalera774583-maker <zebvalera774583@gmail.com>
2026-01-10 14:57:25 +0300

    chore: simplify next.config to minimal standalone config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  assetPrefix: '',
-  basePath: '',
-};
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
