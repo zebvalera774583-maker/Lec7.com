4b6b5bd101f548175f52c54ee5e2b8276c86800a
zebvalera774583-maker <zebvalera774583@gmail.com>
2026-01-17 16:45:57 +0300

    refactor: move public business page from /~[slug] to /u/[slug] with rewrite

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/~:slug',
+        destination: '/u/:slug',
+      },
+    ]
+  },
 }
 module.exports = nextConfig
